8cf2490eef7d57cc7ec5b455a4fb2ff0c1c269d7
IanKal <147105377+IanKal@users.noreply.github.com>
2024-11-12 16:51:54 -0500

    11/12 school

---
+++ validation.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('form')
+const firstname_input = document.getElementById('firstname-input')
+const email_input = document.getElementById('email-input')
+const password_input = document.getElementById('password-input')
+const repeat_password_input = document.getElementById('repeat_password-input')
+
+form.adddEventListener('submit', (e) =>{
+    //e.preventDefault()
+})
